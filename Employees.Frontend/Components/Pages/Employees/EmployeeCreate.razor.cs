09be1d2dd7f9a3b822f26c68bf45a5affa293399
SrGalvis <carlosgalvis06@gmail.com>
2025-10-12 16:03:41 -0500

    Crud frontend Employees

--- Employees.Frontend/Components/Pages/Employees/EmployeeCreate.razor.cs
+++ Employees.Frontend/Components/Pages/Employees/EmployeeCreate.razor.cs
@@ -1,6 +1,36 @@
-﻿
+﻿using Employees.Frontend.Repositories;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Employees.Shared.Entities;
+using System.Diagnostics.Metrics;
+
 namespace Employees.Frontend.Components.Pages.Employees;
 
 public partial class EmployeeCreate
 {
+    private Employee employee = new();
+
+    [Inject] private IRepository Repository { get; set; } = null!;
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
+
+    private async Task CreateAsync()
+    {
+        var responseHttp = await Repository.PostAsync("/api/Employees", employee);
+        if (responseHttp.Error)
+        {
+            var message = await responseHttp.GetErrorMessageAsync();
+            Snackbar.Add(message!, Severity.Error);
+            return;
+        }
+
+        Return();
+        Snackbar.Add("Registro creado", Severity.Success);
+    }
+
+    private void Return()
+    {
+        NavigationManager.NavigateTo("/employees");
+    }
+
 }
---
+++ Employees.Frontend/Components/Pages/Employees/EmployeeForm.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Employees.Shared.Entities;
+using System.Diagnostics.Metrics;
+
+namespace Employees.Frontend.Components.Pages.Employees;
+
+public partial class EmployeeForm
+{
+    private EditContext editContext = null!;
+
+    [EditorRequired, Parameter] public Employee Employees { get; set; } = null!;
+    [EditorRequired, Parameter] public EventCallback OnValidSubmit { get; set; }
+    [EditorRequired, Parameter] public EventCallback ReturnAction { get; set; }
+
+    protected override void OnInitialized()
+    {
+        editContext = new(Employees);
+    }
+}
